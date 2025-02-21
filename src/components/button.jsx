4eeb9fc2c3c16f157eb2882238bdb5f0d4c5dd67
Preetham Praveen <preethampraveencode26@gmail.com>
2025-02-21 08:55:19 +0530

    react-2

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,17 @@
 // write button card here
+import React from 'react'
+
+
+function button({ props }) {
+  return (
+    <div id="hello">
+      <img src={props.img} alt="User" />
+      <h3>{props.name}</h3>
+      <p>Price: {props.price}</p>
+      <button>View product</button>
+      
+    </div>
+  )
+}
+
+export default button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,22 @@
 // write product card here
+import React from 'react';
+import img from "../assets/react.svg"
+import Button from "./button"
+import '../App.css'
+
+
+function productCard() {
+    const checker={
+        img: img,
+        name: 'JQG Phone',
+        price: '32,000',
+        
+    }
+  return (
+    <div>
+      <Button props={checker}/>
+    </div>
+  );
+}
+
+export default productCard;
